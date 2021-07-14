adf1398ec630a7761d8f79d0fbe01f13394a3f5e
mantaikas9 <mantaikas16@gmail.com>
2021-07-14 16:58:24 +0300

    making a check for login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from 'react'; 
+import React, { useState } from 'react'; 
 import Login from './components/Login';
  
 function App() {
+  // let isLoggedIn = false;
+  const [loggedIn, setLoggedIn] = useState(false)
+  if (!loggedIn)
+    return <Login setLoggedIn = {setLoggedIn}/>
+
   return (
-    <div className="App">
-      <Login />
-    </div>
-  );
+      <div className="App">
+        <h1>You made it!</h1>
+      </div>
+    );
+  
 }
  
 export default App;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Login = () => {
+const Login = ({ setLoggedIn }) => {
+      const email = useFormInput('');
+      const password = useFormInput('');
+
+      const handleLogin = () => {
+        if (email.value === "test" && password.value === "test123")
+        {
+          alert("You are an admin!");
+          setLoggedIn = true;
+        }
+        else
+        {
+          alert("bye bye!");
+        }
+      }
+
       return (
         <div>
           <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css" />
+          {/* <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+          <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> */}
           {/*---- Include the above in your HEAD tag --------*/}
           <div className="wrapper fadeInDown">
             <div id="formContent">
               {/* Tabs Titles */}
               {/* Login Form */}
               <form>
-                <input type="text" id="login" className="fadeIn second" name="login" placeholder="login" />
-                <input type="text" id="password" className="fadeIn third" name="login" placeholder="password" />
-                <input type="submit" className="fadeIn fourth" defaultValue="Log In" onClick = { () => console.log('Hey you pressed submit!')}/>
+                <input type="text" id="login" className="fadeIn second" name="login" placeholder="login" {...email} />
+                <input type="text" id="password" className="fadeIn third" name="login" placeholder="password" {...password}/>
+                <button type="submit" className="button" onClick={() => handleLogin()} children='CLICK'>Log In</button>
               </form>
             </div>
           </div>
@@ -20,4 +37,15 @@ const Login = () => {
       );
   }
 
+  const useFormInput = initialValue => {
+    const [value, setValue] = useState(initialValue);
+   
+    const handleChange = e => {
+      setValue(e.target.value);
+    }
+    return {
+      value,
+      onChange: handleChange
+    }
+  }
   export default Login;
